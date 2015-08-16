450abb48974f10bfd2d9478e7ea1b9d87f004fb9
Jordan Harband <ljharb@gmail.com>
2015-08-16 01:50:51 -0700

    Make some things a bit more robust.

--- shim.js
+++ shim.js
@@ -4,15 +4,19 @@ var supportsDescriptors = require('define-properties').supportsDescriptors;
 var getPolyfill = require('./polyfill');
 var gOPD = Object.getOwnPropertyDescriptor;
 var defineProperty = Object.defineProperty;
+var TypeErr = TypeError;
+var getProto = Object.getPrototypeOf;
+var regex = /a/;
 
 module.exports = function shimFlags() {
-	if (!supportsDescriptors) {
-		throw new TypeError('RegExp.prototype.flags requires a true ES5 environment that supports property descriptors');
+	if (!supportsDescriptors || !getProto) {
+		throw new TypeErr('RegExp.prototype.flags requires a true ES5 environment that supports property descriptors');
 	}
 	var polyfill = getPolyfill();
-	var descriptor = gOPD(RegExp.prototype, 'flags');
+	var proto = getProto(regex);
+	var descriptor = gOPD(proto, 'flags');
 	if (!descriptor || descriptor.get !== polyfill) {
-		defineProperty(RegExp.prototype, 'flags', {
+		defineProperty(proto, 'flags', {
 			configurable: true,
 			enumerable: false,
 			get: polyfill
