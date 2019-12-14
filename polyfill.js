13a9fc9d6bc2600681eb3f638668beccf80b843c
Jordan Harband <ljharb@gmail.com>
2019-12-14 13:47:14 -0800

    [Dev Deps] update `eslint`, `@ljharb/eslint-config`, `covert`, `has`, `tape`

--- polyfill.js
+++ polyfill.js
@@ -10,7 +10,7 @@ module.exports = function getPolyfill() {
 	if (!supportsDescriptors) {
 		throw new TypeErr('RegExp.prototype.flags requires a true ES5 environment that supports property descriptors');
 	}
-	if (/a/mig.flags === 'gim') {
+	if ((/a/mig).flags === 'gim') {
 		var descriptor = gOPD(RegExp.prototype, 'flags');
 		if (descriptor && typeof descriptor.get === 'function' && typeof (/a/).dotAll === 'boolean') {
 			return descriptor.get;
